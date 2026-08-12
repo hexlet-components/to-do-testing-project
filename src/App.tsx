d25f7f6417afa86949d186464606661213844e2e
Kirill Mokevnin <mokevnin@gmail.com>
2026-08-11 22:30:42 -0400

    style: format with oxfmt
    Pure formatting, no behaviour change.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import type React from 'react';
-import TaskList from './components/TaskList';
-import './App.css';
+import type React from "react";
+import TaskList from "./components/TaskList";
+import "./App.css";
 
 const App: React.FC = () => {
   return (
